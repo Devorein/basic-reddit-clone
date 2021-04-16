4d0351c6e47ff3a5e977346bf8e4c38cbd6e11ee
Safwan Shaheer <devorein00@gmail.com>
2021-04-16 19:38:23 +0600

    server: Configured mikroorm

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import { MikroORM } from '@mikro-orm/core';
+import { __PROD__ } from './constants';
+
+async function main () {
+	const orm = await MikroORM.init({
+		dbName: 'lireddit',
+		user: 'postgres',
+		password: 'postgres',
+		type: 'postgresql',
+		debug: !__PROD__
+	});
+}
+
+main();
