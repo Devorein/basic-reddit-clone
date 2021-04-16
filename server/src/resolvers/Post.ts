a9f31380d4c325eed76e046a90d12476e66523ce
Safwan Shaheer <devorein00@gmail.com>
2021-04-16 22:08:49 +0600

    Created all crud resolvers for Post

--- server/src/resolvers/Post.ts
+++ server/src/resolvers/Post.ts
@@ -1,4 +1,4 @@
-import { Ctx, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from 'type-graphql';
 import Post from '../entities/Post';
 import { Context } from '../types';
 
@@ -7,5 +7,54 @@ export class PostResolver {
 	@Query(() => [ Post ])
 	posts (@Ctx() ctx: Context) {
 		return ctx.em.find(Post, {});
+	}
+
+	@Query(() => Post, { nullable: true })
+	post (
+		@Arg('id', () => Int)
+		id: number,
+		@Ctx() ctx: Context
+	) {
+		return ctx.em.findOne(Post, { id });
+	}
+
+	@Mutation(() => Post)
+	async createPost (
+		@Arg('title', () => String)
+		title: string,
+		@Ctx() ctx: Context
+	) {
+		const post = ctx.em.create(Post, { title });
+		await ctx.em.persistAndFlush(post);
+		return post;
+	}
+
+	@Mutation(() => Post, { nullable: true })
+	async updatePost (
+		@Arg('id', () => Int)
+		id: number,
+		@Arg('title', () => String)
+		title: string,
+		@Ctx() ctx: Context
+	) {
+		const post = await ctx.em.findOne(Post, { id });
+		if (!post) return null;
+		post.title = title;
+		await ctx.em.persistAndFlush(post);
+		return post;
+	}
+
+	@Mutation(() => Boolean)
+	async deletePost (
+		@Arg('id', () => Int)
+		id: number,
+		@Ctx() ctx: Context
+	) {
+		try {
+			await ctx.em.nativeDelete(Post, { id });
+			return true;
+		} catch (err) {
+			return false;
+		}
 	}
 }
