13e56b981b8c800fe9b6c8583bd6e82a48fbfbde
Safwan Shaheer <devorein00@gmail.com>
2021-04-16 22:58:50 +0600

    Setup redis client and store for express session

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,11 @@
 import { MikroORM } from '@mikro-orm/core';
 import { ApolloServer } from 'apollo-server-express';
+import connectRedis from 'connect-redis';
 import express from 'express';
+import expressSession from 'express-session';
+import redis from 'redis';
 import { buildSchema } from 'type-graphql';
+import { __PROD__ } from './constants';
 import ormConfig from './mikro-orm.config';
 import { PostResolver } from './resolvers/Post';
 import { UserResolver } from './resolvers/User';
@@ -12,13 +16,33 @@ async function main () {
 
 	const app = express();
 
+	const RedisStore = connectRedis(expressSession);
+	const RedisClient = redis.createClient();
+
+	app.use(
+		expressSession({
+			name: 'qid',
+			store: new RedisStore({ client: RedisClient, disableTouch: true, disableTTL: true }),
+			cookie: {
+				maxAge: 1000 * 60 * 60 * 24 * 30, // a month
+				httpOnly: true,
+				secure: __PROD__,
+				sameSite: 'lax'
+			},
+			secret: 'redis_secret',
+			resave: false
+		})
+	);
+
 	const apolloServer = new ApolloServer({
 		schema: await buildSchema({
 			resolvers: [ PostResolver, UserResolver ],
 			validate: false
 		}),
-		context: () => ({
-			em: orm.em
+		context: ({ req, res }) => ({
+			em: orm.em,
+			req,
+			res
 		})
 	});
 
--- server/src/types.ts
+++ server/src/types.ts
@@ -1,5 +1,8 @@
 import { Connection, EntityManager, IDatabaseDriver } from '@mikro-orm/core';
+import { Request, Response } from 'express';
 
 export type Context = {
+	req: Request;
+	res: Response;
 	em: EntityManager<IDatabaseDriver<Connection>>;
 };
