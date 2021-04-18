9c6436a611cf1565d572eb3442cc01236f39ab19
Safwan Shaheer <devorein00@gmail.com>
2021-04-18 20:24:45 +0600

    client: Created useMeQuery

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,20 +1,33 @@
 import { Box, Flex, Link } from '@chakra-ui/react';
 import NextLink from "next/link";
 import React from 'react';
+import { useMeQuery } from '../generated/graphql';
 
 interface NavbarProps {
 
 }
 
 const Navbar: React.FC<NavbarProps> = ({ }) => {
-  return (<Flex bg="tomato" p={4}>
-    <Box ml="auto" color="white">
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  if (fetching) {
+
+  } else if (!data?.me) {
+    body = <>
       <NextLink href="/register">
         <Link mr="4">Register</Link>
       </NextLink>
       <NextLink href="/login">
         <Link mr="4">Login</Link>
       </NextLink>
+    </>
+  } else {
+    body = <Box>{data.me.username}</Box>
+  }
+  return (<Flex bg="tomato" p={4}>
+    <Box ml="auto" color="white">
+      {body}
     </Box>
   </Flex>);
 }
