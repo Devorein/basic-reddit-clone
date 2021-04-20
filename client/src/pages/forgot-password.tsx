8ef14f809d458e7948e4b438c309b4571a3f5a1a
Safwan Shaheer <devorein00@gmail.com>
2021-04-20 23:16:45 +0600

    client: Added ForgotPassword Component

--- client/src/pages/forgot-password.tsx
+++ client/src/pages/forgot-password.tsx
@@ -1,7 +1,23 @@
+import { Button } from '@chakra-ui/react';
+import { Form, Formik } from 'formik';
 import React from 'react';
+import InputField from '../components/InputField';
+import { useForgotPasswordMutation } from '../generated/graphql';
 
 const ForgotPassword: React.FC<{}> = ({ }) => {
-  return <div>So You forgot your password</div>;
+  const [, forgotPassword] = useForgotPasswordMutation()
+  return (
+    <Formik initialValues={{ email: '' }} onSubmit={async (values) => {
+      forgotPassword({ email: values.email });
+    }}>
+      {({ isSubmitting }) =>
+        <Form>
+          <InputField name="email" placeholder="email" label="Email" />
+          <Button isLoading={isSubmitting} colorScheme="teal" m={5} type="submit">Forgot Password?</Button>
+        </Form>
+      }
+    </Formik>
+  )
 }
 
 export default ForgotPassword
