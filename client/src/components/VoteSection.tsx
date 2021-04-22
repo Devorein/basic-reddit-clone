9c48f86f48c588cc13d7035224d3620bb614274c
Safwan Shaheer <devorein00@gmail.com>
2021-04-22 16:15:47 +0600

    c: Added loading state logic for vote buttons

--- client/src/components/VoteSection.tsx
+++ client/src/components/VoteSection.tsx
@@ -1,6 +1,6 @@
 import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons';
 import { Box, Flex, IconButton } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { PostInfoFragment, useVoteMutation } from '../generated/graphql';
 
 interface Props {
@@ -8,20 +8,31 @@ interface Props {
 }
 
 export default function VoteSection({ post }: Props) {
+  const [loadingState, setLoadingState] = useState<'upvote-loading' | 'not-loading' | 'downvote-loading'>('not-loading');
+  console.log(loadingState);
   const [, vote] = useVoteMutation();
   return (
     <Flex p={5} shadow="md" borderWidth="1px" direction="column" justifyContent="center" alignItems="center">
-      <IconButton onClick={() => {
-        vote({
+      <IconButton onClick={async () => {
+        setLoadingState('upvote-loading');
+        await vote({
           postId: post.id,
           value: 1
         })
-      }} aria-label="Up Vote post" colorScheme="tomato" size="sm">
-        <ChevronUpIcon size="50px" cursor="pointer" color="white" />
+        setLoadingState('not-loading');
+      }} isLoading={loadingState !== 'not-loading'} aria-label="Up Vote post" bg="red.500" size="sm">
+        <ChevronUpIcon size="50px" cursor="pointer" color="black" />
       </IconButton>
       <Box m={3}>{post.points}</Box>
-      <IconButton aria-label="Down Vote post" colorScheme="tomato" size="sm">
-        <ChevronDownIcon size="50px" cursor="pointer" color="white" />
+      <IconButton onClick={async () => {
+        setLoadingState('downvote-loading');
+        await vote({
+          postId: post.id,
+          value: -1
+        })
+        setLoadingState('not-loading');
+      }} isLoading={loadingState !== 'not-loading'} aria-label="Down Vote post" bg="red.500" size="sm">
+        <ChevronDownIcon size="50px" cursor="pointer" color="black" />
       </IconButton>
     </Flex>
   )
