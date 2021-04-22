402dcf8095cc018a5aca9f0c9e40f6353e24d129
Safwan Shaheer <devorein00@gmail.com>
2021-04-22 15:44:19 +0600

    c: Created Upvote buttons

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Button, Flex, Heading, Stack, Text } from "@chakra-ui/react";
+import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons';
+import { Box, Button, Flex, Heading, IconButton, Stack, Text } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
 import NextLinK from "next/link";
 import React, { useState } from "react";
@@ -20,11 +21,23 @@ const Index = () => {
     <Layout>
       {!fetching && meData?.me && <NextLinK href="/create-post"><Button bg="tomato" m={5}>Create Post</Button></NextLinK>}
       {fetchingPosts ? <div>Loading ...</div> : postsData && <Stack spacing={5}> {postsData.posts.posts.map(post =>
-        <Box key={post.id} p={5} shadow="md" borderWidth="1px">
-          <Heading fontSize="xl">{post.title}</Heading>
-          <Heading fontSize="sm">by {post.creator.username}</Heading>
-          <Text mt={4}>{post.textSnippet}</Text>
-        </Box>)}
+        <Flex key={post.id} >
+          <Flex p={5} shadow="md" borderWidth="1px" direction="column" justifyContent="center" alignItems="center">
+            <IconButton aria-label="Up Vote post" bg="tomato" size="sm">
+              <ChevronUpIcon size="50px" cursor="pointer" color="white" />
+            </IconButton>
+            <Box>{post.points}</Box>
+            <IconButton aria-label="Down Vote post" bg="tomato" size="sm">
+              <ChevronDownIcon size="50px" cursor="pointer" color="white" />
+            </IconButton>
+          </Flex>
+          <Box flex="1" p={5} shadow="md" borderWidth="1px">
+            <Heading fontSize="xl">{post.title}</Heading>
+            <Heading fontSize="sm">by {post.creator.username}</Heading>
+            <Text mt={4}>{post.textSnippet}</Text>
+          </Box>
+        </Flex>
+      )}
       </Stack>}
       {postsData && postsData.posts.hasMore && <Flex>
         <Button onClick={() => {
